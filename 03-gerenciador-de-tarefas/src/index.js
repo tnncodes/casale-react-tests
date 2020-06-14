4f4df462b1fcfb5365ab8992d445a5604e3df755
theandersonn <theandersonn@gmail.com>
2020-06-14 12:33:42 -0300

    proj#03 - Update mudança de rotas

--- 03-gerenciador-de-tarefas/src/index.js
+++ 03-gerenciador-de-tarefas/src/index.js
@@ -6,12 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <GerenciadorTarefas />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<GerenciadorTarefas />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
