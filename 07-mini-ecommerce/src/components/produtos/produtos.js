705712bd969062bd11d8497b7f2d65ac6265f593
theandersonn <theandersonn@gmail.com>
2020-06-16 18:38:12 -0300

    proj#07 - criação inicial dos componentes

---
+++ 07-mini-ecommerce/src/components/produtos/produtos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Produtos() {
+  return <h1>Produtos</h1>;
+}
+
+export default Produtos;
--- 07-mini-ecommerce/src/mini-ecommerce.js
+++ 07-mini-ecommerce/src/mini-ecommerce.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./mini-ecommerce.css";
 
+import Menu from "./components/menu/menu";
+import Produtos from "./components/produtos/produtos";
+import Checkout from "./components/checkout/checkout";
+
 function MiniEcommerce() {
-  return <h1>MiniEcommerce</h1>;
+  const [carrinho, setCarrinho] = useState({ produtos: [] });
+  const [exibirProdutos, setExibirProdutos] = useState(true);
+  const [exibirCheckout, setExibirCheckout] = useState(false);
+  const [total, setTotal] = useState("0,00");
+
+  return (
+    <>
+      <Menu />
+      <Produtos />
+      <Checkout />
+    </>
+  );
 }
 
 export default MiniEcommerce;
