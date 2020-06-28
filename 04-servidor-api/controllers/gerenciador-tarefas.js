b7ba30a856729dacc12ae9b8c316411a6ffbb9f9
theandersonn <theandersonn@gmail.com>
2020-06-28 15:37:58 -0300

    proj#08 - upload

--- 04-servidor-api/controllers/gerenciador-tarefas.js
+++ 04-servidor-api/controllers/gerenciador-tarefas.js
@@ -1,140 +1,114 @@
-const { uuid } = require("uuidv4");
+const uuidv4 = require('uuid/v4');
 
 let tarefas = [
-  { id: "1", nome: "Aprender React", concluida: true },
-  { id: "2", nome: "Aprender Node", concluida: false },
-  { id: "3", nome: "Estudar padrões de projetos", concluida: false },
-  { id: "4", nome: "Estudar React com hooks", concluida: false },
+  { id: '1', nome: 'Aprender React', concluida: true },
+  { id: '2', nome: 'Estudar padrões de projetos', concluida: false },
+  { id: '3', nome: 'Aprender Javascript', concluida: false },
+  { id: '4', nome: 'Estudar React usando hooks', concluida: false }
 ];
 
 function listarTarefaId(req, res) {
   const id = req.params.id;
-  const tarefa = tarefas.filter((tarefa) => tarefa.id === id);
-
+  const tarefa = tarefas.filter(tarefa => tarefa.id === id);
   if (tarefa.length === 0) {
-    res.status(404).json({ erro: "Tarefa não encontrada." });
+    res.status(404).json({ erro: 'Tarefa não encontrada.' });
   }
-
   res.json(tarefa[0]);
 }
 
 function listarTarefas(req, res) {
-  const pagina = req.query["pag"] || 1;
-  const ordem = req.query["ordem"]; // ASC, DESC
-  const filtroTarefa = req.query["filtro-tarefa"];
-  const itensPorPagina = req.query["itens-por-pagina"] || 3;
+  const pagina = req.query['pag'] || 1;
+  const ordem = req.query['ordem']; // ASC, DESC
+  const filtroTarefa = req.query['filtro-tarefa'];
+  const itensPorPagina = req.query['itens-por-pagina'] || 3;
   let tarefasRetornar = tarefas.slice(0);
-
-  // filtro
+  // filtrar
   if (filtroTarefa) {
     tarefasRetornar = tarefasRetornar.filter(
-      (t) => t.nome.toLowerCase().indexOf(filtroTarefa.toLowerCase()) === 0
-    );
+      t => t.nome.toLowerCase().indexOf(filtroTarefa.toLowerCase()) === 0);
   }
-
   // ordenar
-  if (ordem === "ASC") {
-    tarefasRetornar.sort((t1, t2) =>
-      t1.nome.toLowerCase() > t2.nome.toLowerCase() ? 1 : -1
-    );
-  } else if (ordem === "DESC") {
-    tarefasRetornar.sort((t1, t2) =>
-      t2.nome.toLowerCase() > t1.nome.toLowerCase() ? 1 : -1
-    );
+  if (ordem === 'ASC') {
+    tarefasRetornar.sort((t1, t2) => (t1.nome.toLowerCase() > t2.nome.toLowerCase()) ? 1 : -1);
+  } else if (ordem === 'DESC') {
+    tarefasRetornar.sort((t1, t2) => (t1.nome.toLowerCase() < t2.nome.toLowerCase()) ? 1 : -1);
   }
-
   // retornar
   res.json({
     totalItens: tarefasRetornar.length,
-    tarefas: tarefasRetornar
-      .slice(0)
-      .splice((pagina - 1) * itensPorPagina, itensPorPagina),
-    pagina: pagina,
+    tarefas: tarefasRetornar.slice(0).splice((pagina - 1) * itensPorPagina, itensPorPagina),
+    pagina: pagina
   });
 }
 
 function cadastrarTarefa(req, res) {
-  if (!req.body["nome"] && !req.body["concluida"]) {
-    res.status(400).json({ erro: "Requisição inválida" });
+  if (!req.body['nome'] && !req.body['concluida']) {
+    res.status(400).json({ erro: 'Requisição inválida.' });
   }
-
   const tarefa = {
-    id: uuid(),
-    nome: req.body["nome"],
-    concluida: req.body["concluida"],
+    id: uuidv4(),
+    nome: req.body['nome'],
+    concluida: req.body['concluida']
   };
-
   tarefas.push(tarefa);
   res.json(tarefa);
 }
 
 function atualizarTarefa(req, res) {
-  if (!req.body["nome"] && !req.body["concluida"]) {
-    res.status(400).json({ erro: "Requisição inválida." });
+  if (!req.body['nome'] && !req.body['concluida']) {
+    res.status(400).json({ erro: 'Requisição inválida.' });
   }
-
   const id = req.params.id;
   let tarefaAtualizada = false;
-
-  tarefas = tarefas.map((tarefa) => {
+  tarefas = tarefas.map(tarefa => {
     if (tarefa.id === id) {
-      tarefa.nome = req.body["nome"];
-      tarefa.concluida = req.body["concluida"];
+      tarefa.nome = req.body['nome'];
+      tarefa.concluida = req.body['concluida'];
       tarefaAtualizada = true;
     }
-
     return tarefa;
   });
-
   if (!tarefaAtualizada) {
-    res.status(404).json({ erro: "Tarefa não encontrada." });
+    res.status(404).json({ erro: 'Tarefa não encontrada.' });
   }
-
   res.json({
     id: id,
-    nome: req.body["nome"],
-    concluida: req.body["concluida"],
+    nome: req.body['nome'],
+    concluida: req.body['concluida']
   });
 }
 
 function removerTarefa(req, res) {
   const id = req.params.id;
   const numTarefas = tarefas.length;
-
-  tarefas = tarefas.filter((tarefa) => tarefa.id !== id);
-
+  tarefas = tarefas.filter(tarefa => tarefa.id !== id);
   if (numTarefas === tarefas.length) {
-    res.status(404).json({ erro: "Tarefa não encontrada." });
+    res.status(404).json({ erro: 'Tarefa não encontrada.' });
   }
-
-  res.json({ msg: "Tarefa removida com sucesso!" });
+  res.json({ msg: 'Tarefa removida com sucesso!' });
 }
 
 function concluirTarefa(req, res) {
   const id = req.params.id;
   let tarefaConcluida = false;
-
-  tarefas = tarefas.map((tarefa) => {
+  tarefas = tarefas.map(tarefa => {
     if (tarefa.id === id) {
       tarefa.concluida = true;
       tarefaConcluida = true;
     }
-
     return tarefa;
   });
-
   if (!tarefaConcluida) {
-    res.status(404).json({ erro: "Tarefa não encontrada." });
+    res.status(404).json({ erro: 'Tarefa não encontrada.' });
   }
-
-  res.json({ msg: "Tarefa concluida com sucesso!" });
+  res.json({ msg: 'Tarefa concluída com sucesso!' });
 }
 
 module.exports = {
-  listarTarefas,
   listarTarefaId,
+  listarTarefas,
   cadastrarTarefa,
   atualizarTarefa,
   removerTarefa,
-  concluirTarefa,
-};
+  concluirTarefa
+}
--- 04-servidor-api/index.js
+++ 04-servidor-api/index.js
@@ -1,47 +1,46 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const {
-  listarTarefas,
   listarTarefaId,
+  listarTarefas,
   cadastrarTarefa,
   atualizarTarefa,
   removerTarefa,
-  concluirTarefa,
-} = require("./controllers/gerenciador-tarefas.js");
-
+  concluirTarefa
+} = require('./controllers/gerenciador-tarefas.js');
 const {
-  finalizarCompra,
-  obterCidadesPorEstado,
-} = require("./controllers/mini-ecommerce.js");
+  finalizarCompra, obterCidadesPorEstado
+} = require('./controllers/mini-ecommerce.js');
+const upload = require('./controllers/upload.js');
 
 const app = express();
 const port = 3001;
 
+app.use(express.static('public'));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(fileUpload({ createParentPath: true }));
 
-// listar todas as tarefas - (get)
-app.get("/gerenciador-tarefas", listarTarefas);
-
-// listar uma tarefa por id - (get)
-app.get("/gerenciador-tarefas/:id", listarTarefaId);
-
-// cadastrar uma tarefa - (post)
-app.post("/gerenciador-tarefas", cadastrarTarefa);
-
-// atualizar uma tarefa - (put)
-app.put("/gerenciador-tarefas/:id", atualizarTarefa);
-
-// remover uma tarefa - (delete)
-app.delete("/gerenciador-tarefas/:id", removerTarefa);
-
-// concluir uma tarefa - (put)
-app.put("/gerenciador-tarefas/:id/concluir", concluirTarefa);
+// listar todas as tarefas - get
+app.get('/gerenciador-tarefas', listarTarefas);
+// listar uma tarefa por id - get
+app.get('/gerenciador-tarefas/:id', listarTarefaId);
+// cadastrar uma tarefa - post
+app.post('/gerenciador-tarefas', cadastrarTarefa);
+// atualizar uma tarefa - put
+app.put('/gerenciador-tarefas/:id', atualizarTarefa);
+// remover uma tarefa - delete
+app.delete('/gerenciador-tarefas/:id', removerTarefa);
+// concluir uma tarefa - put
+app.put('/gerenciador-tarefas/:id/concluir', concluirTarefa);
 
 // mini-ecommerce
-app.post("/mini-ecommerce/checkout/finalizar-compra", finalizarCompra);
-app.get("/mini-ecommerce/estado/:siglaEstado/cidades", obterCidadesPorEstado);
+app.post('/mini-ecommerce/checkout/finalizar-compra', finalizarCompra);
+app.get('/mini-ecommerce/estado/:siglaEstado/cidades', obterCidadesPorEstado);
+
+app.post('/upload', upload);
 
 app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
